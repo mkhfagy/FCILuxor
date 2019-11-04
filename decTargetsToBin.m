5c70dbee6693ce09986a395febadf2900ffa350f
Muhammad Atta Othman Ahmed <muahann@gmail.com>
2019-11-04 11:32:00 +0200

    Update decTargetsToBin.m

--- decTargetsToBin.m
+++ decTargetsToBin.m
@@ -1,3 +1,5 @@
+%% Function to convert decimal values to a binary format, By: Dr/Muhammad Ahmed Khfagy, FCL Luxor.
+% Discrete Structure / Level 2 .
 function [ binTargets ] = decTargetsToBin( decTargets )
 c=length(unique(decTargets));
 binTargets=zeros(c,length(decTargets));
